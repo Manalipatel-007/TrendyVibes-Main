ea21d87705ba5bf582a23b1b56dca09950b1a24e
Manali Verma <manalipatel123456789@gmail.com>
2024-12-21 22:08:19 +0530

    Add Address Card

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddressCard = ()=>{
+    return(
+        <div>
+            <div className="space-y-3">
+                <p className="font-semibold">Raam Kapoor</p>
+                <p>Bhopal, Sidhi Vinayak colony, 462030</p>
+                <div className="space-y-1">
+                    <p font-semibold>Phone Number</p>
+                    <p>7685947372</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddressCard;
